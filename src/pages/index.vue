bd69522933ca9663684b2be12be2ab74e06e0033
xxljunjun <liaojunjun@autel.com>
2021-06-18 15:44:05 +0800

    0

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <view>
+  <view class="home">
     <Top title="Home" :isAdd="true" />
-    <view v-if="hasData"></view>
-    <view v-else>
+    <view v-if="hasData" class="hasdata"></view>
+    <view v-else class="nodatabox">
       <image src="/static/icons/nodata.png" class="nodata" />
+      <text>无内容</text>
     </view>
     <TabBar current="index" />
   </view>
