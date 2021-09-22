2fb12489557ab6a3c3ad03b17501c1a6e7ef264c
xxljunjun <liaojunjun@autel.com>
2021-09-22 08:58:31 +0800

    ma地图的使用

--- src/pages/mine.vue
+++ src/pages/mine.vue
@@ -1,22 +1,53 @@
 <template>
   <view class="mine"
-    ><Top title="Me" :isAdd="true" />3333333<TabBar current="mine"
-  /></view>
+    ><Top title="Me" :isAdd="true" />
+    <TabBar current="mine" />
+    <view>
+      <view class="page-body">
+        <view class="page-section page-section-gap">
+          <map
+            style="width: 100%; height: 300px;"
+            :latitude="latitude"
+            :longitude="longitude"
+            :markers="covers"
+          >
+          </map>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-import Top from '@/pages/component/top.vue'
-import TabBar from '@/pages/component/tabBar.vue'
+import Top from "@/pages/component/top.vue";
+import TabBar from "@/pages/component/tabBar.vue";
 export default {
   components: {
     TabBar,
     Top,
   },
   data() {
-    return {}
+    return {
+      id: 0, // 使用 marker点击事件 需要填写id
+      title: "map",
+      latitude: 39.909,
+      longitude: 116.39742,
+      covers: [
+        {
+          latitude: 39.909,
+          longitude: 116.39742,
+          iconPath: "../static/add.png",
+        },
+        {
+          latitude: 39.9,
+          longitude: 116.39,
+          iconPath: "../static/add.png",
+        },
+      ],
+    };
   },
   methods: {},
-}
+};
 </script>
 
 <style lang="scss" scoped>
